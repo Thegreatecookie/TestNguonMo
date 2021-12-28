24871282fca1f6ef31ca56b587dbd96bf1275c42
Thegreatecookie <hatrikhang@yahoo.com.vn>
2021-12-28 17:55:12 +0700

    Them the loai

--- controllers/theloaicontroller.php
+++ controllers/theloaicontroller.php
@@ -7,4 +7,13 @@ if ($action=='index')
     include './views/theloai/theloai.php';
 
 }
+
+if($action=='them'){
+    $id_theloai=trim($_POST['id_theloai']);
+    $tentheloai=trim($_POST['tentheloai']);
+    
+        $data=$theloai->insert($id_theloai,$tentheloai);
+        header('location: ./index.php?controller=theloaicontroller&action=index');
+}
+
 ?>
