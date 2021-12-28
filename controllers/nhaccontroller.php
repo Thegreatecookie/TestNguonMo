0c00a1624e29578be09413a55563440e7134a4f4
Thegreatecookie <hatrikhang@yahoo.com.vn>
2021-12-28 17:40:15 +0700

    Sua nhac

--- controllers/nhaccontroller.php
+++ controllers/nhaccontroller.php
@@ -16,4 +16,20 @@ if($action=='them')
     $data=$nhac->insert($id_nhac,$tennhac,$video,$tentacgia);
     header('location: ./index.php?controller=nhaccontroller&action=index');
 }
+if($action=='sua')
+{   
+    $id_nhac = isset($_GET['id'])?$_GET['id']:'';
+    $data =$nhac->hiensua($id_nhac);
+    include './views/nhac/suanhac.php';
+}
+
+if($action=='xulysua')
+{
+    $tennhac=$_POST['tennhac'];
+    $video=time().'-'.$_FILES['video']['name'];
+    move_uploaded_file($_FILES['video']['tmp_name'], "views/assets/video/$video");
+    $tentacgia=$_POST['tentacgia'];
+    $data=$nhac->suaNhac($tennhac,$video,$tentacgia);
+    header('location: ./index.php?controller=nhaccontroller&action=index');
+}
 ?>
