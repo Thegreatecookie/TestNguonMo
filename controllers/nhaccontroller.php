f38063ca8400f450b4274dd3670768ea9bb40140
Thegreatecookie <hatrikhang@yahoo.com.vn>
2021-12-28 17:38:23 +0700

    Them nhac

--- controllers/nhaccontroller.php
+++ controllers/nhaccontroller.php
@@ -6,4 +6,14 @@ if ($action=='index')
     $data =$nhac->all();
     include './views/nhac/nhac.php';
 }
+if($action=='them')
+{
+    $id_nhac=$_POST['id_nhac'];
+    $tennhac=$_POST['tennhac'];
+    $tentacgia=$_POST['tentacgia'];
+    $video=time().'-'.$_FILES['video']['name'];
+    move_uploaded_file($_FILES['video']['tmp_name'], "views/assets/video/$video");
+    $data=$nhac->insert($id_nhac,$tennhac,$video,$tentacgia);
+    header('location: ./index.php?controller=nhaccontroller&action=index');
+}
 ?>
