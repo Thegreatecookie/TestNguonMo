5e54d4c5c0aa0afcf9955d6d283aeddb064553e4
Thegreatecookie <hatrikhang@yahoo.com.vn>
2021-12-28 17:24:54 +0700

    Sua san pham

--- controllers/sanphamcontroller.php
+++ controllers/sanphamcontroller.php
@@ -21,6 +21,26 @@ if($action=='them'){
   $data=$sanpham->insert($id_dianhac,$tendianhac,$mota,$gia,$hinh,$id_theloai);
   header('location: ./index.php?controller=sanphamcontroller&action=index');
 }
+
+if($action=='sua')
+{   
+    $id_dianhac = isset($_GET['id'])?$_GET['id']:'';
+    $data =$sanpham->hiensua($id_dianhac);
+    $dataTL=$theloai->all();
+    include './views/sanpham/suasanpham.php';
+}
+
+if($action=='xulysua')
+{
+    $tendianhac=$_POST['tendianhac'];
+    $mota=$_POST['mota'];
+    $gia=$_POST['gia'];
+    $hinh=time().'-'.$_FILES['hinh']['name'];
+    move_uploaded_file($_FILES['hinh']['tmp_name'], "views/assets/img/$hinh");
+    $id_theloai=$_POST['id_theloai'];
+    $data=$sanpham->suaSP($id_dianhac,$tendianhac,$mota,$gia,$hinh,$id_theloai);
+    header('location: ./index.php?controller=sanphamcontroller&action=index');
+}
 ?>
 
 
