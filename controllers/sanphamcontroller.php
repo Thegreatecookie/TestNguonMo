e3a92f60e555b4b69f317287e9cb69edcc30d908
Thegreatecookie <hatrikhang@yahoo.com.vn>
2021-12-28 17:23:16 +0700

    Them san pham

--- controllers/sanphamcontroller.php
+++ controllers/sanphamcontroller.php
@@ -9,6 +9,18 @@ if ($action=='index')
  
     include './views/sanpham/sanpham.php';
 }
+
+if($action=='them'){
+  $id_dianhac=$_POST['id_dianhac'];
+  $tendianhac=$_POST['tendianhac'];
+  $mota=$_POST['mota'];
+  $gia=$_POST['gia'];
+  $hinh=time().'-'.$_FILES['hinh']['name'];
+  move_uploaded_file($_FILES['hinh']['tmp_name'], "views/assets/img/$hinh");
+  $id_theloai=$_POST['id_theloai'];
+  $data=$sanpham->insert($id_dianhac,$tendianhac,$mota,$gia,$hinh,$id_theloai);
+  header('location: ./index.php?controller=sanphamcontroller&action=index');
+}
 ?>
 
 
